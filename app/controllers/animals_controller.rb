45e0113ed5630d2ff3ce336c6f92a2ac758f2c84
Renzlawr <RSLogan23@gmail.com>
2022-06-16 18:18:56 -0600

    CRUD

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,42 @@
 class AnimalsController < ApplicationController
+
+    def index
+        animals = Animal.all
+        render json: animals
+    end
+
+    def show
+        animal = Animal.find(params[:id])
+        render json: animal
+    end
+
+    def create
+        animal = Animal.create(animal_params)
+        if animal.valid?
+            render json: animal
+        else
+            render json: animal.errors
+        end
+    end
+
+    def update
+        animal = Animal.find(params[:id])
+        animal.update(animal_params)
+        if animal.valid?
+            render json: animal
+        else
+            render json: animal.errors
+        end
+    end
+
+    def destroy
+        animal = Animal.find(params[:id])
+        animal.destroy
+    end
+
+    private
+    def animal_params
+        params.require(:animal).permit(:common_name, :latin_name, :kingdom)
+    end
+
 end
